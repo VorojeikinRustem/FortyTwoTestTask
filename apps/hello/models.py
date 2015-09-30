1e2019a031e207021a0e810d5e16a2a520d19eec
te7ris <vorojeikin.rustem@gmail.com>
2015-09-30 03:05:08 +0300

    1 create test, models

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,16 @@
+
 from django.db import models
 
-# Create your models here.
+
+class MyProfile(models.Model):
+    
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    skype = models.CharField(max_length=50)
+    bio = models.TextField()
+    other_contacts = models.TextField()
+
+    def __unicode__(self):
+        return self.name + ' ' + self.last_name
--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -2,8 +2,13 @@ from django.test import TestCase
 
 # Create your tests here.
 
+from django.http import HttpRequest
 
 class SomeTests(TestCase):
-    def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 5)
+
+    def test_page_profile(self):
+        request = HttpRequest()
+        response = index(request)
+        self.assertTrue(response.content.startswith(b'<html>')) 
+        self.assertIn(b'<title>42 Coffee Cups Test Assignment</title>', response.content) 
+        self.assertTrue(response.content.endswith(b'</html>'))  
